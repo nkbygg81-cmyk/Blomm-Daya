056c46eacb1bb0ea3da28cbecb8c42c62b190a77
nazar <nkbygg81@gmail.com>
2026-02-17 23:07:04 +0100

    add missing AdminPanelScreen

---
+++ screens/AdminPanelScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function AdminPanelScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Admin Panel</Text>
+    </View>
+  );
+}
